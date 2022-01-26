cb6ae4af42f914e89ccc0e6c1e665b4d25d845e0
iqranaz240 <iqranaz240@gmail.com>
2022-01-26 16:41:31 +0500

    updated

--- Redux Principle/index.js
+++ Redux Principle/index.js
@@ -1,7 +1,13 @@
 const redux = require('redux')
+const reduxLogger = require('redux-logger')
+
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+const logger = reduxLogger.createLogger()
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUYICEREAM'
 
 function buyCake(){
     return{
@@ -10,26 +16,60 @@ function buyCake(){
     }
 }
 
-// (prevState, action) => newState
-const initialState = {
-    numOfCakes: 10
+function buyIcecream(){
+    return{
+        type: BUY_ICECREAM
+    }
 }
 
-const reducer = (state = initialState, action) => {
+// (prevState, action) => newState
+// const initialState = {
+//     numOfCakes: 10,
+//     numOfIcecreams : 20
+// }
+
+const initialCakeState = {
+    numOfCakes: 10,
+}
+
+const initialIcecreamState = {
+    numOfIcecreams : 20
+}
+
+const cakeReducer = (state = initialCakeState, action) => {
     switch(action.type){
         case BUY_CAKE: return{
             ...state,
             numOfCakes: state.numOfCakes -1
         }
+
         default: return state
     }
 }
 
-const store =createStore(reducer);
+const icecreamReducer = (state = initialIcecreamState, action) => {
+    switch(action.type){
+        case BUY_ICECREAM: return{
+            ...state,
+            numOfIcecreams: state.numOfIcecreams -1
+        }
+
+        default: return state
+    }
+}
+
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: icecreamReducer
+})
+
+const store =createStore(rootReducer, applyMiddleware(logger));
 console.log('Initial state', store.getState())
-const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
+const unsubscribe = store.subscribe(() => {})
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIcecream())
+store.dispatch(buyIcecream())
 store.dispatch(buyCake())
 unsubscribe()
 
